51cfc116a6c2739898577f702dae98caa47d9dd4
CallisteHanriat <couim38@gmail.com>
2016-05-13 22:17:12 +0200

    Add design + changing page title + initial froze number

--- count.js
+++ count.js
@@ -1,3 +1,5 @@
+var name_of_page;
+
 function change_like_number(number_likes) {
   var elem = document.querySelector('#number_of_likes');
   od = new Odometer({
@@ -9,17 +11,18 @@ function change_like_number(number_likes) {
     duration : 3000
   });
   elem.innerHTML = number_likes;
+  document.title = name_of_page + " " + number_likes;
 }
 
 function startCounter() {
   pageId = $("#inputPageId").val();
   console.log("pageId = " + pageId);
-  update.live;
+  set_initial_state();
   setInterval(update.live, 1*1000);
 }
+
 function onClickSubmitButton() {
   actionWithLoginOrNot();
-
 }
 
 var update={};
@@ -29,7 +32,38 @@ update.live = function() {
     'GET',
     {"fields":"fan_count"},
     function(response) {
-        change_like_number(response.fan_count);
+      change_like_number(response.fan_count);
+    }
+  );
+}
+
+function set_initial_state () {
+  FB.api(
+    '/'+pageId+'/',
+    'GET',
+    {"fields":"fan_count"},
+    function(response) {
+      var initdoc = document.querySelector('#initial_number');
+      od2 = new Odometer({
+        el: initdoc,
+        value: 200,
+
+        // Any option (other than auto and selector) can be passed in here
+        format: '( ddd),dd',
+        duration : 3000
+      });
+      initdoc.innerHTML = response.fan_count;
+
+      FB.api(
+        '/'+pageId+'/',
+        'GET',
+        function(response) {
+          $("#text_initial_value").text("Initial value of " + response.name +  " was ");
+          name_of_page = response.name;
+        }
+      );
+
+      change_like_number(response.fan_count);
     }
   );
 }
@@ -40,14 +74,14 @@ function actionWithLoginOrNot() {
         // the user is logged in and has authenticated your
         // app, and response.authResponse supplies
         // the user's ID, a valid access token, a signed
-        // request, and the time the access token 
+        // request, and the time the access token
         // and signed request each expire
         console.log("je suis vrai");
         isCo =  1;
         startCounter();
-        
+
       } else if (response.status === 'not_authorized') {
-        // the user is logged in to Facebook, 
+        // the user is logged in to Facebook,
         // but has not authenticated your app
         console.log("je suis moyen vrai");
         isCo =  1;
@@ -70,13 +104,13 @@ function actionWithLoginOrNot() {
         // the user is logged in and has authenticated your
         // app, and response.authResponse supplies
         // the user's ID, a valid access token, a signed
-        // request, and the time the access token 
+        // request, and the time the access token
         // and signed request each expire
         var uid = response.authResponse.userID;
         var accessToken = response.authResponse.accessToken;
-        
+
       } else if (response.status === 'not_authorized') {
-        // the user is logged in to Facebook, 
+        // the user is logged in to Facebook,
         // but has not authenticated your app
       } else {
         // the user isn't logged in to Facebook.
@@ -104,9 +138,6 @@ function onload() {
     onClickSubmitButton();
   });
   $("#loginButton").on("click", function(){
-    login(); 
-    setInterval(update.live, 1*1000); 
+    login();
   });
 }
-
-
