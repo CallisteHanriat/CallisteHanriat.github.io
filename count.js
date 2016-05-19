70cc54c7d644f3eb38f7f7dbe209a3024d1d2d43
CallisteHanriat <callistehanriat@gmail.com>
2016-05-19 17:38:46 +0200

    start counter after login

--- count.js
+++ count.js
@@ -95,19 +95,18 @@ function actionWithLoginOrNot() {
 
 }
 
-
 function login() {
     FB.login(function(response) {
-              if (response.authResponse) {
-                   console.log('Welcome!  Fetching your information.... ');
-                   FB.api('/me', function(response) {
-                   console.log('Good to see you, ' + response.name + '.');
-                  // findAccessToken(response);
-                });
-               } else {
-                console.log('User cancelled login or did not fully authorize.');
-               }
-           }, {scope: 'email,user_likes'});
+      if (response.authResponse) {
+         console.log('Welcome!  Fetching your information.... ');
+         FB.api('/me', function(response) {
+         console.log('Good to see you, ' + response.name + '.');
+      });
+     } else {
+        console.log('User cancelled login or did not fully authorize.');
+     }
+   }, {scope: 'email,user_likes'});
+   startCounter();
 }
 
 function onload() {
